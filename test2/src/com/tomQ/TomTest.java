105e00bdeaddd7455bdc895a21363282d9c28149
hahah <2346543654@163.com>
2018-01-22 17:03:22 +0800

    copy

--- test2/src/com/tomQ/TomTest.java
+++ test2/src/com/tomQ/TomTest.java
@@ -5,6 +5,7 @@ public class TomTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("I am a  good person!");
+		System.out.println("I am a  good person2!");
 	}
 
 }
