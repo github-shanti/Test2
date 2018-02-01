383aea34b387bf337accf182f8cd79b32665e93e
hahah <2346543654@163.com>
2018-02-01 14:03:02 +0800

    sumbmit test

--- test2/src/test2/TestMain.java
+++ test2/src/test2/TestMain.java
@@ -1,10 +1,20 @@
 package test2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TestMain {
 
 	public static void main(String[] args) {
-		System.out.println("这是中文");
-		System.out.println(2);
+		
+		Pattern p = Pattern.compile("cat");
+		 Matcher m = p.matcher("one cat two cats in the yard");
+		 StringBuffer sb = new StringBuffer();
+		 while (m.find()) {
+		     m.appendReplacement(sb, "dog");
+		 }
+		 m.appendTail(sb);
+		 System.out.println(sb.toString());
 	}
 
 }
