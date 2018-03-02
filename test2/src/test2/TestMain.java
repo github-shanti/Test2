f07614b858e27a46d60157b2f0159a0b4f2fc33b
hahah <2346543654@163.com>
2018-03-02 13:55:29 +0800

    pring now

--- test2/src/test2/TestMain.java
+++ test2/src/test2/TestMain.java
@@ -1,20 +1,10 @@
 package test2;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class TestMain {
 
 	public static void main(String[] args) {
 		
-		Pattern p = Pattern.compile("cat");
-		 Matcher m = p.matcher("one cat two cats in the yard");
-		 StringBuffer sb = new StringBuffer();
-		 while (m.find()) {
-		     m.appendReplacement(sb, "dog");
-		 }
-		 m.appendTail(sb);
-		 System.out.println(sb.toString());
+		System.out.println("now");
 	}
 
 }
